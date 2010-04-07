74c4e99d2980f9d9fc6e8cee504e365473b46a74
RavanH <RavanH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-07 10:58:03 +0000

    single/double quotes repolish
    git-svn-id: https://plugins.svn.wordpress.org/highlight-search-terms/trunk@226053 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hlst.php
+++ hlst.php
@@ -84,16 +84,16 @@ function hlst_extend() {
   var hlst_query  = new Array(<?php echo implode(",",$filtered); ?>);
   jQuery.fn.extend({
     highlight: function(search, insensitive, span_class){
-      var regex = new RegExp("(<[^>]*>)|("+ search.replace(/([-.*+?^${}()|[\]\/\\])/g,"\\$1") +")", insensitive ? "ig" : "g");
+      var regex = new RegExp('(<[^>]*>)|('+ search.replace(/([-.*+?^${}()|[\]\/\\])/g,"\\$1") +')', insensitive ? 'ig' : 'g');
       return this.html(this.html().replace(regex, function(a, b, c){
-        return (a.charAt(0) == "<") ? a : "<span class=\""+ span_class +"\">" + c + "</span>";
+        return (a.charAt(0) == '<') ? a : '<span class="'+ span_class +'">' + c + '</span>';
       }));
     }
   });
   jQuery(document).ready(function($){
     if(typeof(hlst_query) != 'undefined'){
       for (i in hlst_query){
-        $("<?php echo $area; ?>").highlight(hlst_query[i], 1, "hilite term-" + i);
+        $('<?php echo $area; ?>').highlight(hlst_query[i], 1, 'hilite term-' + i);
       }
     }
   });
